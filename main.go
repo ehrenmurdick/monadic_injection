5d2c980b3d8b9a715f9897be3668ee3ccb580e97
Ehren Murdick <emurdick@pivotal.io>
2015-11-06 17:05:12 -0800

    getting pretty monadic

--- main.go
+++ main.go
@@ -4,7 +4,25 @@ import "./repos"
 import "./ent"
 import "./io"
 
-var repo = repos.NewItemRepo()
+func panicErr(err error) error {
+	panic(err.Error())
+	return err
+}
+
+// initialize a monadic repo.
+// failure to open can be handled
+// here by adding:
+// .Handle(panicErr)
+// that would quit the
+// server if the db is configured
+// incorrectly, for example.
+// otherwise, you could defer
+// handling the opening db error
+// until a page request, which is more
+// like what you see with Rails
+// for example.
+var repo = repos.
+	OpenItemRepo()
 
 func main() {
 	var i = ent.Item{
--- repos/item_repo.go
+++ repos/item_repo.go
@@ -3,9 +3,61 @@ package repos
 import "../ent"
 import "errors"
 
+type MaybeItemRepo struct {
+	Value ItemRepo
+	Err   error
+}
+
+func (in MaybeItemRepo) AndThenItem(bl func(ItemRepo) ent.MaybeItem) ent.MaybeItem {
+	if in.Err != nil {
+		return ent.MaybeItem{
+			Err: in.Err,
+		}
+	} else {
+		return bl(in.Value)
+	}
+}
+
+func (in MaybeItemRepo) Get(key int) ent.MaybeItem {
+	if in.Err != nil {
+		return ent.MaybeItem{
+			Err: in.Err,
+		}
+	} else {
+		return in.Value.Get(key)
+	}
+}
+
+func (in MaybeItemRepo) Save(key int, item ent.Item) ent.MaybeItem {
+	if in.Err != nil {
+		return ent.MaybeItem{
+			Err: in.Err,
+		}
+	} else {
+		return in.Value.Save(key, item)
+	}
+}
+
+func (in MaybeItemRepo) Handle(handler func(error) error) MaybeItemRepo {
+	if in.Err != nil {
+		handler(in.Err)
+	}
+	return in
+}
+
 type ItemRepo interface {
 	Get(int) ent.MaybeItem
 	Save(int, ent.Item) ent.MaybeItem
+}
+
+func OpenItemRepo() (out MaybeItemRepo) {
+	out.Value = NewItemRepo()
+	return
+}
+
+func OpenBadItemRepo() (out MaybeItemRepo) {
+	out.Err = errors.New("failed to open item repo")
+	return
 }
 
 func NewItemRepo() ItemRepo {
